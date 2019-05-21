097ffac9a5adb83cca0079029645653380404081
Unknown <amelhassan98@yahoo.com>
2019-05-21 16:40:38 -0400

    Needed scripts for Service Provider added

---
+++ Unity/ARFUROS/Assets/Scripts/CustomRosSharp/TeleopProvider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using rosapi = RosSharp.RosBridgeClient.Services.RosApi;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class TeleopProvider : ServiceProvider<rosapi.GetParamRequest, rosapi.GetParamResponse>
+    {
+        public Type MessageType { get { return (typeof(GeometryTwist)); } }
+        private GeometryTwist rawMessage;
+
+        private void Awake()
+        {
+            MessageReception += ReceiveMessage;
+        }
+
+        protected bool ServiceResponseHandler(rosapi.GetParamRequest request, out rosapi.GetParamResponse response){
+        	
+        }
+
+        private void UpdateValues()
+        {
+            rawMessage.linear.x = 2.2;
+            rawMessage.linear.y = 0.0;
+            rawMessage.linear.z = 0.0;
+            rawMessage.angular.x = 0.0;
+            rawMessage.angular.y = 0.0;
+            rawMessage.angular.z = 0.0;
+
+        }
+    }
+}
